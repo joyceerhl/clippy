d825909cc0d5625c058f4e55388a0534a57b23c0
Felix Rieseberg <fr@makenotion.com>
2025-05-02 09:49:53 -0700

    Force sha256 only

--- forge.config.ts
+++ forge.config.ts
@@ -49,10 +49,11 @@ const FLAGS = {
   APPLE_ID_PASSWORD: process.env.APPLE_ID_PASSWORD,
 };
 
-const windowsSign = {
+const windowsSign: any = {
   signToolPath: FLAGS.SIGNTOOL_PATH,
   signWithParams: `/v /dlib ${FLAGS.AZURE_CODE_SIGNING_DLIB} /dmdf ${FLAGS.AZURE_METADATA_JSON_PATH}`,
   timestampServer: "http://timestamp.acs.microsoft.com",
+  hashes: ["sha256"],
 };
 
 function setup() {
